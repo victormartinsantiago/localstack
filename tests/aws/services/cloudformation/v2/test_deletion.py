4a76f6d39086562e057d94c139137839133f5c97
Simon Walker <simon.walker@localstack.cloud>
2025-05-02 23:01:50 +0100

    Implement deletion of single resources

---
+++ tests/aws/services/cloudformation/v2/test_deletion.py
@@ -0,0 +1,31 @@
+import os
+
+import pytest
+
+from localstack.testing.pytest import markers
+from localstack.utils.strings import short_uid
+
+
+@markers.aws.validated
+@markers.snapshot.skip_snapshot_verify(paths=["$..message"])
+def test_single_resource(deploy_cfn_template, aws_client, snapshot):
+    value = short_uid()
+    snapshot.add_transformer(snapshot.transform.regex(value, "<value>"))
+    stack = deploy_cfn_template(
+        template_path=os.path.join(
+            os.path.dirname(__file__), "../../../templates/ssm_parameter_defaultname.yaml"
+        ),
+        parameters={"Input": value},
+    )
+
+    parameter_name = stack.outputs["CustomParameterOutput"]
+    snapshot.add_transformer(snapshot.transform.regex(parameter_name, "<parameter-name>"))
+    value = aws_client.ssm.get_parameter(Name=parameter_name)["Parameter"]
+    snapshot.match("get-value", value)
+
+    stack.destroy()
+
+    with pytest.raises(aws_client.ssm.exceptions.ParameterNotFound) as exc_info:
+        aws_client.ssm.get_parameter(Name=parameter_name)
+
+    snapshot.match("exc-value", exc_info.value.response)
